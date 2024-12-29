26098257e5b455b6bcd83118d843e7f9bdf7a9ad
sakthivel182006 <156519241+sakthivel182006@users.noreply.github.com>
2024-12-29 22:09:10 +0530

    Create product.route.js

---
+++ backend/product.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { createproduct, deleteproduct, getProducts, putproduct } = require("./product.controller.js");  // Ensure correct path
+
+const router = express.Router();  // Initialize the router
+
+// Define routes using const
+router.get('/', getProducts);
+router.post('/', createproduct);
+router.put('/:id', putproduct);
+router.delete('/:id', deleteproduct);
+
+module.exports = router;  // Export router to use in server.js
